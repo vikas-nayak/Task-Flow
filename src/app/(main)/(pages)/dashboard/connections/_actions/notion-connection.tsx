03188951a0e19cc2996f86368ff68a5ea7919b70
Vikas Nayak <vikasnayak96999@gmail.com>
2024-09-11 15:21:21 +0530

    Discord, Notion and Slack Connections added. ShadCN components installed

--- src/app/(main)/(pages)/dashboard/connections/_actions/notion-connection.tsx
+++ src/app/(main)/(pages)/dashboard/connections/_actions/notion-connection.tsx
@@ -5,59 +5,50 @@ import { currentUser } from '@clerk/nextjs/server'
 import { Client } from '@notionhq/client'
 
 export const onNotionConnect = async (
-    access_token: string,
-    workspace_id: string,
-    workspace_icon: string,
-    workspace_name: string,
-    database_id: string,
-    id: string
-  ) => {
-    'use server'
-    if (access_token) {
-      try {
-        const user = await currentUser()
-        console.log('Current User:', user)
-        
-        const notion_connected = await db.notion.findFirst({
-          where: {
-            accessToken: access_token,
+  access_token: string,
+  workspace_id: string,
+  workspace_icon: string,
+  workspace_name: string,
+  database_id: string,
+  id: string
+) => {
+  'use server'
+  if (access_token) {
+    //check if notion is connected
+    const notion_connected = await db.notion.findFirst({
+      where: {
+        accessToken: access_token,
+      },
+      include: {
+        connections: {
+          select: {
+            type: true,
           },
-          include: {
-            connections: {
-              select: {
-                type: true,
-              },
-            },
-          },
-        })
-  
-        if (!notion_connected) {
-          await db.notion.create({
-            data: {
+        },
+      },
+    })
+
+    if (!notion_connected) {
+      //create connection
+      await db.notion.create({
+        data: {
+          userId: id,
+          workspaceIcon: workspace_icon!,
+          accessToken: access_token,
+          workspaceId: workspace_id!,
+          workspaceName: workspace_name!,
+          databaseId: database_id,
+          connections: {
+            create: {
               userId: id,
-              workspaceIcon: workspace_icon!,
-              accessToken: access_token,
-              workspaceId: workspace_id!,
-              workspaceName: workspace_name!,
-              databaseId: database_id,
-              connections: {
-                create: {
-                  userId: id,
-                  type: 'Notion',
-                },
-              },
+              type: 'Notion',
             },
-          })
-          console.log('Notion connected')
-        }
-      } catch (error) {
-        console.error("Error in onNotionConnect:", error)
-      }
+          },
+        },
+      })
     }
   }
-  
-  
-  
+}
 export const getNotionConnection = async () => {
   const user = await currentUser()
   if (user) {
