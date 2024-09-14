a321b2d50c39c63ee0341289e6efc5c84c0cddbc
Vikas Nayak <vikasnayak96999@gmail.com>
2024-09-14 17:50:30 +0530

    Setup for Ngrok public url  and  google drive listener api Now I can see and listen to any changes in drive of the user at .../api/drive

---
+++ src/app/api/drive-activity/route.ts
@@ -0,0 +1,79 @@
+import { google } from 'googleapis'
+import { auth, clerkClient } from '@clerk/nextjs/server'
+import { NextResponse } from 'next/server'
+import { v4 as uuidv4 } from 'uuid'
+import { db } from '@/lib/db'
+
+export async function GET() {
+  const oauth2Client = new google.auth.OAuth2(
+    process.env.GOOGLE_CLIENT_ID,
+    process.env.GOOGLE_CLIENT_SECRET,
+    process.env.OAUTH2_REDIRECT_URI
+  )
+
+  const { userId } = auth()
+  if (!userId) {
+    return NextResponse.json({ message: 'User not found' })
+  }
+
+  try {
+    const clerkResponse = await clerkClient().users.getUserOauthAccessToken(userId, 'oauth_google')
+    // console.log('Clerk Response:', clerkResponse)  // Debugging line
+
+    // Extract the token properly
+    const accessToken = clerkResponse.data[0]?.token
+    if (!accessToken) {
+      throw new Error('Access token not found')
+    }
+
+    oauth2Client.setCredentials({
+      access_token: accessToken,
+    })
+
+    const drive = google.drive({
+      version: 'v3',
+      auth: oauth2Client,
+    })
+    
+    const channelId = uuidv4()
+
+    const startPageTokenRes = await drive.changes.getStartPageToken({})
+    const startPageToken = startPageTokenRes.data.startPageToken
+    if (startPageToken == null) {
+      throw new Error('startPageToken is unexpectedly null')
+    }
+
+    const listener = await drive.changes.watch({
+      pageToken: startPageToken,
+      supportsAllDrives: true,
+      supportsTeamDrives: true,
+      requestBody: {
+        id: channelId,
+        type: 'web_hook',
+        address:
+          `${process.env.NGROK_URI}/api/drive-activity/notification`,
+        kind: 'api#channel',
+      },
+    })
+
+    if (listener.status == 200) {
+      const channelStored = await db.user.updateMany({
+        where: {
+          clerkId: userId,
+        },
+        data: {
+          googleResourceId: listener.data.resourceId,
+        },
+      })
+
+      if (channelStored) {
+        return new NextResponse('Listening to changes...')
+      }
+    }
+  } catch (error) {
+    // console.error('Error:', error)  // Debugging line
+    return new NextResponse('Oops! something went wrong, try again')
+  }
+
+  return new NextResponse('Oops! something went wrong, try again')
+}
